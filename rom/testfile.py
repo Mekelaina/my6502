7a11e8ee68b35c40f38d911b263ebdf6efa7271b
chloe hausser <chloerae018@gmail.com>
2023-05-02 19:44:02 -0400

    created a simple testrom to implement loading roms

---
+++ rom/testfile.py
@@ -0,0 +1,27 @@
+
+
+signature = [0x4D, 0x59, 0x36, 0x35, 0x30, 0x32]
+
+reset_low  = 0xFFFC
+reset_high = 0xFFFD
+
+lowbyte = 0x00
+
+highbyte = 0x80
+
+content = []
+content += signature
+print(content)
+
+for x in range(0xFFFF+1):
+    print(x)
+    if x == reset_low:
+        content.append(lowbyte)
+    elif x == reset_high:
+        content.append(highbyte)
+    else:
+        content.append(0x00)
+
+with open("testrom.my6502", "wb") as file:
+    c = bytearray(content)
+    file.write(c)
